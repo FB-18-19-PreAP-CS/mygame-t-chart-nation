30cd3b37bdaebecec58fc846761dce5f5d73c624
Nathan D. Black <2210262@flourbluffschools.org>
2019-04-29 14:11:04 -0500

    Door has no attribute rect

--- mainGame.py
+++ mainGame.py
@@ -14,8 +14,11 @@ class Game:
         self.clock = pygame.time.Clock()
         # self.bgmusic = pygame.mixer.Sound('/home/blackn/preAPCS/mygame_purdy/sounds/main_theme.ogg')
         self.character = Hero()
-        # self.room_types = ['street','forrest']
         self.curr_type = 0
+        self.existingdoors = []
+        image1 = pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/dunegon.png')
+        self.room1 = Room([Door(30,40), Door(650, 200)], image1)
+        self.currentRoom = self.room1
 
     def start(self):
         done = False
@@ -36,17 +39,29 @@ class Game:
             if pressed[pygame.K_RIGHT]:
                 self.character.move('right')
 
-            self.draw_bg()
+            self.draw_bg(self.currentRoom.background)
             self.character.draw(self.screen)
             # self.character.blitme(self.screen)
             pygame.display.flip()
+
+            self.checkCollisions()
 
             # self.check_edge()
 
             self.clock.tick(60)
 
-    def draw_bg(self):
-        self.screen.blit(pygame.image.load('Screenshot_20190409_140137.png'),(0,0))
+    def checkCollisions(self):
+        for door in self.existingdoors:
+            if self.character.rect.colliderect(door.rect):
+                print("eyy") #test
+
+    def loadRoom(Room):
+        self.draw_bg(Room.background)
+        for door in self.doorList:
+            self.existingdoors.append(door)
+
+    def draw_bg(self, image):
+        self.screen.blit(pygame.transform.scale(image, (800,800)),(0,0))
 
 class Hero:
     def __init__(self):
@@ -54,6 +69,7 @@ class Hero:
         self.y = 20
         self.frame = 0
         self.animation = pygame.transform.scale((pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-idle-00.png')), (120,120))
+        self.orientation = "right"
         self.moveRight = []
         for i in range(6):
             self.moveRight.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-run-0{i}.png'), (120,120)))
@@ -65,7 +81,10 @@ class Hero:
 
     def draw(self,screen):
         f = int(self.frame)%6
-        screen.blit(self.moveRight[f],(self.rect.x,self.rect.y))
+        if self.orientation == "right":
+            screen.blit(self.moveRight[f],(self.rect.x,self.rect.y))
+        else:
+            screen.blit(pygame.transform.flip(self.moveRight[f],True,False),(self.rect.x,self.rect.y))
 
     def move(self, direction):
         if direction == "up":
@@ -73,10 +92,27 @@ class Hero:
         elif direction == "down":
             self.rect.y += 5
         elif direction == "left":
+            self.orientation = "left"
             self.rect.x -= 5
         else:
+            self.orientation = "right"
             self.rect.x += 5
         self.frame += .25
+
+class Door:
+    def __init__(self, spawnx, spawny):
+        self.x = spawnx
+        self.y = spawny
+        self.rect.x = 120
+        self.rect.y = 120
+
+    
+class Room:
+    def __init__(self, doorList, background):
+        self.background = background
+        
+        
+
 
 session = Game()
 session.start()
