bb87f3a7873492150f17a7c29349041ee59663dc
Christian-Cormier <43218064+Christian-Cormier@users.noreply.github.com>
2019-04-29 14:12:20 -0500

    Add files via upload

--- mygame.py
+++ mygame.py
@@ -10,9 +10,9 @@ pygame.display.set_caption("Dungeon Master")
 walkright = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkleft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 
-bg = pygame.image.load('bg.jpg')
+bg = pygame.image.load('R9.png')
 char = pygame.image.load('standing.png')
-tile = pygame.image.load('generic-rpg-tile07.png')
+tile = pygame.image.load('bg.jpg')
 
 clock = pygame.time.Clock()
 x = 50 
@@ -23,15 +23,32 @@ vel = 5
 left = False
 right = False
 walkcount = 0
+game_intro = True
 
+font = pygame.font.Font('freesansbold.ttf',32)
+text = font.render('Dungeon Master', True,(255,255,255),(0,0,0))
+text1 = font.render('Press Backspace to continue',True,(255,255,255),(0,0,0))
 
-
-def home_screen:
-    for i in range(0,500):
-        for j in range(0,500):
-            disp.blit(bg, (i,j))
-            
-
+def home_screen():
+    # for i in range(0,500):
+    #     for j in range(0,500):
+    #         disp.blit(bg, (i,j))
+    while game_intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_BACKSPACE]:
+        game_intro = False
+    else:
+        textrect = text.get_rect()
+        textrect.center = (500//2,475//2)
+        disp.blit(text,textrect)
+        textrect1 = text.get_rect()
+        textrect1.center = (150,400)
+        disp.blit(text1,textrect1)
+        pygame.display.update
 
 
 
@@ -55,11 +72,10 @@ def redrawgamewindow():
 run = True
 while run:
     clock.tick(27)
-    
+    home_screen()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] and x > vel:
         x -= vel
@@ -81,7 +97,4 @@ while run:
         walkcount = 0
     redrawgamewindow()
     
-    
 pygame.quit()
-
-https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
