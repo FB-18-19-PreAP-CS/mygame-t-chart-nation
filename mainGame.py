13e97cd9db7adde8f30231f0beb0b6664b2d32e8
Nathan D. Black <nathanblack262@gmail.com>
2019-05-01 07:01:03 -0500

    random room generation functional

--- mainGame.py
+++ mainGame.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from random import choice
 
 WIDTH = 800
 HEIGHT = 800
@@ -13,24 +14,44 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Stalin")
         self.clock = pygame.time.Clock()
-        # self.bgmusic = pygame.mixer.Sound('/home/blackn/preAPCS/mygame_purdy/sounds/main_theme.ogg')
+        self.bgmusic = pygame.mixer.music.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/escape!.mp3')
         self.character = Hero()
         self.curr_type = 0
         self.existingdoors = []
-        image1 = pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/dunegon.png')
+        image1 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/dunegon.png')
+        image2 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos.jpg')
+        image3 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/endgame.png')
+        image4 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos3.jpg')
+        image5 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos4.jpg')
+        image2 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos5.jpg')
+        image6 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos6.jpg')
+        image7 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/thanos7.png')
+        image8 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/loss.png')
+        image9 = pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/loss2.jpg')
         self.room1 = Room([Door(60,360), Door(700,360)], image1)
-        self.room2 = Room([Door(50,200), Door(600,260)], image1)
-        self.currentRoom = self.room1
+        self.room2 = Room([Door(60,360), Door(700,360)], image2)
+        self.room3 = Room([Door(60,360), Door(700,360)], image3)
+        self.room4 = Room([Door(60,360), Door(700,360)], image4)
+        self.room5 = Room([Door(60,360), Door(700,360)], image5)
+        self.room6 = Room([Door(60,360), Door(700,360)], image6)
+        self.room7 = Room([Door(60,360), Door(700,360)], image7)
+        self.room8 = Room([Door(60,360), Door(700,360)], image8)
+        self.room9 = Room([Door(60,360), Door(700,360)], image9)
+        self.room10 = Room([Door(60,360), Door(700,360)], image1)
+        self.roomList = [self.room1, self.room2, self.room3, self.room4, self.room5, self.room6, self.room7, self.room8, self.room9, self.room10]
+        self.currentRoom = choice(self.roomList)
 
     def start(self):
         done = False
-        # self.bgmusic.play(-1)
+        pygame.mixer.music.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/escape!.mp3')
+        pygame.mixer.music.play(-1)
         self.pastRoom = self.currentRoom
         self.loadRoom(self.currentRoom)
         while not done:
             self.screen.fill((0,0,0))
             if self.currentRoom != self.pastRoom:
                 self.loadRoom(self.currentRoom)
+                self.pastRoom = self.currentRoom
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
@@ -49,15 +70,10 @@ class Game:
             self.character.draw(self.screen)
             for door in self.existingdoors:
                 door.draw(self.screen)
-            # self.character.blitme(self.screen)
             pygame.display.flip()
 
             if self.checkCollisions() == True:
-                self.pastRoom = self.currentRoom
-                if self.currentRoom == self.room1:
-                    self.currentRoom = self.room2
-                else:
-                    self.currentRoom = self.room2
+                self.currentRoom = choice(self.roomList)
 
 
             # self.check_edge()
@@ -73,8 +89,10 @@ class Game:
     def loadRoom(self, Room):
         time.sleep(2)
         self.draw_bg(Room.background)
+        self.existingdoors = []
         for door in Room.doorList:
             self.existingdoors.append(door)
+        self.character.draw(self.screen)
 
     def draw_bg(self, image):
         self.screen.blit(pygame.transform.scale(image, (800,800)),(0,0))
@@ -84,11 +102,11 @@ class Hero:
         self.x = 50
         self.y = 20
         self.frame = 0
-        self.animation = pygame.transform.scale((pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-idle-00.png')), (120,120))
+        self.animation = pygame.transform.scale((pygame.image.load('C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/images/adventurer-idle-00.png')), (120,120))
         self.orientation = "right"
         self.moveRight = []
         for i in range(6):
-            self.moveRight.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-run-0{i}.png'), (120,120)))
+            self.moveRight.append(pygame.transform.scale(pygame.image.load(f'C:/Users/13612/Academics/preAPCS/mygame-t-chart-nation/images/adventurer-run-0{i}.png'), (120,120)))
         self.rect = self.animation.get_rect()
         self.rect.x = 120
         self.rect.y = 120
