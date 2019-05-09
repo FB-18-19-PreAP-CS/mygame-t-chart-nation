c88416b7a8bb5f1928c8a8cbad95a2787d4b4441
NathanBlack262 <43217968+NathanBlack262@users.noreply.github.com>
2019-05-09 14:11:39 -0500

    main menu works, powerup bugs resolved

--- mainGame.py
+++ mainGame.py
@@ -4,12 +4,19 @@ from random import choice, randint
 
 WIDTH = 800
 HEIGHT = 800
+EDGEXL = 85
+EDGEXR = WIDTH - 170
+EDGEYT = 85
+EDGEYB = HEIGHT - 200
 
 class Game:
     def __init__(self):
         pygame.mixer.pre_init(48000,-16,2,2048)
         pygame.mixer.init()
         pygame.init()
+
+        self.font = pygame.font.Font('freesansbold.ttf',32)
+        self.text = self.font.render('Dungeon Master', True,(255,255,255),(0,0,0))
 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Stalin")
@@ -19,7 +26,6 @@ class Game:
         self.existingwalls = []
         self.existingItems = []
         image1 = pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/dunegon.png')
-        # Door pattern: left right up down
         self.room1 = Room([LockedDoor(35,360), Door(700,360), HiddenDoor(300,300)], [], image1)
         self.room2 = Room([LockedDoor(35,360), Door(700,360)], [], image1)
         self.room3 = Room([LockedDoor(35,360), Door(700,360)], [], image1)
@@ -30,11 +36,53 @@ class Game:
         self.room8 = Room([LockedDoor(35,360), Door(700,360)], [], image1)
         self.room9 = Room([LockedDoor(35,360), Door(700,360)], [], image1)
         self.room10 = Room([LockedDoor(35,360), Door(700,360)], [], image1)
-        self.hiddenRoom = HiddenRoom([],[],image1)
+        self.hiddenRoom = HiddenRoom([Door(700,360)],[],image1)
         self.roomList = [self.room1, self.room2, self.room3, self.room4, self.room5, self.room6, self.room7, self.room8, self.room9, self.room10]
         self.currentRoom = self.room1 #choice(self.roomList)
         self.exploredRoomList = []
         self.exploredRoomList.append(self.currentRoom)
+
+    def text_objects(self,text, font):
+        textsurface = font.render(text, True, (255,255,255))
+        return textsurface, textsurface.get_rect()
+    
+    def button(self,msg,x,y,w,h,ic,ac,action = None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + w > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(self.screen, ac,(x,y,w,h))
+
+            if click[0] == 1 and action != None:
+                action()
+        else:
+            pygame.draw.rect(self.screen, ic,(x,y,w,h))
+
+        smalltext = pygame.font.Font("freesansbold.ttf",20)
+        textsurf, textrect = self.text_objects(msg,smalltext)
+        textrect.center = ( (x+(w/2)), (y+(h/2)) )
+        self.screen.blit(textsurf, textrect)
+    
+    def quitgame(self):
+        pygame.quit()
+        quit()
+    
+    def game_intro(self):
+        intro = True
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+            self.screen.fill((0,0,0))
+            textrect = self.text.get_rect()
+            textrect.center = (WIDTH//2,300)
+            self.screen.blit(self.text,textrect)
+            self.button('GO!',200,400,100,50,(0,255,0),(0,200,0),self.start)
+            self.button("Quit!",500,400,100,50,(255,0,0),(200,0,0),self.quitgame)
+
+            pygame.display.update()
+            self.clock.tick(60)
 
     def start(self):
         done = False
@@ -58,15 +106,23 @@ class Game:
                 if pressed[pygame.K_UP]:
                     if not self.checkEdges("up") and not self.checkWalls("up"):
                         self.character.move('up')
+                    elif self.checkEdges("up") == True:
+                        self.character.rect.y = EDGEYT
                 if pressed[pygame.K_DOWN]:
                     if not self.checkEdges("down") and not self.checkWalls("down"):
                         self.character.move('down')
+                    elif self.checkEdges("down") == True:
+                        self.character.rect.y = EDGEYB
                 if pressed[pygame.K_LEFT]:
                     if not self.checkEdges("left") and not self.checkWalls("left"):
                         self.character.move('left')
+                    elif self.checkEdges("left") == True:
+                        self.character.rect.x = EDGEXL
                 if pressed[pygame.K_RIGHT]:
                     if not self.checkEdges("right") and not self.checkWalls("right"):
                         self.character.move('right')
+                    elif self.checkEdges("right") == True:
+                        self.character.rect.x = EDGEXR
             if pressed[pygame.K_SPACE]:
                 self.character.attacking = True
                 self.character.attackAnimation = 0
@@ -132,40 +188,40 @@ class Game:
     def checkWalls(self,direction):
         for wall in self.existingwalls:
             if direction == "up":
-                self.character.rect.y -= 5
+                self.character.rect.y -= (5 + (2.5 * self.character.speed))
                 if self.character.rect.colliderect(wall.rect):
-                    self.character.rect.y += 5
+                    self.character.rect.y += (5 + (2.5 * self.character.speed))
                     return True
-                self.character.rect.y += 5
+                self.character.rect.y += (5 + (2.5 * self.character.speed))
             if direction == "down":
-                self.character.rect.y += 5
+                self.character.rect.y += (5 + (2.5 * self.character.speed))
                 if self.character.rect.colliderect(wall.rect):
-                    self.character.rect.y -= 5
+                    self.character.rect.y -= (5 + (2.5 * self.character.speed))
                     return True
-                self.character.rect.y -= 5
+                self.character.rect.y -= (5 + (2.5 * self.character.speed))
             if direction == "left":
-                self.character.rect.x -= 5
+                self.character.rect.x -= (5 + (2.5 * self.character.speed))
                 if self.character.rect.colliderect(wall.rect):
-                    self.character.rect.x += 5
+                    self.character.rect.x += (5 + (2.5 * self.character.speed))
                     return True
-                self.character.rect.x += 5
+                self.character.rect.x += (5 + (2.5 * self.character.speed))
             if direction == "right":
-                self.character.rect.x += 5
+                self.character.rect.x += (5 + (2.5 * self.character.speed))
                 if self.character.rect.colliderect(wall.rect):
-                    self.character.rect.x -= 5
+                    self.character.rect.x -= (5 + (2.5 * self.character.speed))
                     return True
-                self.character.rect.x -= 5
+                self.character.rect.x -= (5 + (2.5 * self.character.speed))
         return False
 
     def checkEdges(self, direction):
         if direction == "up":
-            if self.character.rect.y < 50:
+            if self.character.rect.y < 85:
                 return True
         elif direction == "down":
-            if self.character.rect.y > HEIGHT-250:
+            if self.character.rect.y > HEIGHT-200:
                 return True
         elif direction == "left":
-            if self.character.rect.x < 50:
+            if self.character.rect.x < 85:
                 return True
         elif direction == "right":
             if self.character.rect.x > WIDTH-170:
@@ -208,17 +264,17 @@ class Hero:
         self.defense = 0
         self.speed = 0
         self.frame = 0
-        self.animation = pygame.transform.scale((pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-idle-00.png')), (120,120))
+        self.animation = pygame.transform.scale((pygame.image.load('/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-idle-00.png')), (80,80))
         self.orientation = "right"
         self.moveRight = []
         self.moveAttack = []
         self.attackAnimation = -1
         self.attacking = False
         for i in range(6):
-            self.moveRight.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-run-0{i}.png'), (120,120)))
+            self.moveRight.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-run-0{i}.png'), (80,80)))
         for i in range(6):
-            self.moveAttack.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-attack2-0{i}.png'), (120,120)))
-        self.rect = self.animation.get_rect()
+            self.moveAttack.append(pygame.transform.scale(pygame.image.load(f'/home/blackn/preAPCS/mygame-t-chart-nation/images/adventurer-attack2-0{i}.png'), (80,80)))
+        self.rect = pygame.Rect(0,0,80,80)
         self.rect.x = 100
         self.rect.y = 310
 
@@ -243,15 +299,15 @@ class Hero:
 
     def move(self, direction):
         if direction == "up":
-            self.rect.y -= 5
+            self.rect.y -= (5 + (1.5 * self.speed))
         elif direction == "down":
-            self.rect.y += 5
+            self.rect.y += (5 + (1.5 * self.speed))
         elif direction == "left":
             self.orientation = "left"
-            self.rect.x -= 5
+            self.rect.x -= (5 + (1.5 * self.speed))
         else:
             self.orientation = "right"
-            self.rect.x += 5
+            self.rect.x += (5 + (1.5 * self.speed)) 
         self.frame += .25
 
     def attack(self, animation, screen):
@@ -277,7 +333,7 @@ class LockedDoor(Door):
 class HiddenDoor(Door):
     def __init__(self,spawnx,spawny):
         super().__init__(spawnx,spawny)
-        self.rect = pygame.Rect(spawnx,spawny,spawnx+50,spawny+50)
+        self.rect = pygame.Rect(spawnx,spawny,50,50)
 
     def draw(self,screen):
         pass
@@ -301,7 +357,7 @@ class Wall:
 
 class Item:
     def __init__(self,sprite):
-        self.sprite = pygame.image.load(f'{sprite}')
+        self.sprite = pygame.transform.scale((pygame.image.load(f'{sprite}')), (25,25))
         self.rect = self.sprite.get_rect()
         self.rect.x,self.rect.y = 250,400
 
@@ -327,4 +383,4 @@ class Defence_up(Item):
 
 
 session = Game()
-session.start()
+session.game_intro()
